7851e67351202901c80a0a23e808d565bc443f7f
SANDRICKSON <sandroqatamadze802@gmail.com>
2025-10-15 16:44:00 +0400

    Add files via upload

---
+++ MANCANA.ino
@@ -0,0 +1,140 @@
+int in1 = 2;
+int in2 = 3;
+int in3 = 4;
+int in4 = 5;
+
+void setup() {
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+
+
+}
+void loop() {
+
+  //კვადრატი
+  //  digitalWrite(in1, HIGH);
+  //  digitalWrite(in2, LOW);
+  //  digitalWrite(in3, HIGH);
+  //  digitalWrite(in4, LOW);
+  //
+  //  delay(1000);
+  //
+  //  digitalWrite(in1, HIGH);
+  //  digitalWrite(in2, LOW);
+  //  digitalWrite(in3, LOW);
+  //  digitalWrite(in4, HIGH);
+  //
+  //  delay(800);
+  //
+  //  digitalWrite(in1, HIGH);
+  //  digitalWrite(in2, LOW);
+  //  digitalWrite(in3, HIGH);
+  //  digitalWrite(in4, LOW);
+  //
+  //  delay(1000);
+  //
+  //  digitalWrite(in1, HIGH);
+  //  digitalWrite(in2, LOW);
+  //  digitalWrite(in3, LOW);
+  //  digitalWrite(in4, HIGH);
+  //
+  //  delay(800);
+  //
+  //  digitalWrite(in1, HIGH);
+  //  digitalWrite(in2, LOW);
+  //  digitalWrite(in3, HIGH);
+  //  digitalWrite(in4, LOW);
+  //
+  //  delay(1000);
+  //
+  //  digitalWrite(in1, HIGH);
+  //  digitalWrite(in2, LOW);
+  //  digitalWrite(in3, LOW);
+  //  digitalWrite(in4, HIGH);
+  //
+  //  delay(800);
+  //
+  //
+  //  digitalWrite(in1, HIGH);
+  //  digitalWrite(in2, LOW);
+  //  digitalWrite(in3, HIGH);
+  //  digitalWrite(in4, LOW);
+  //
+  //  delay(1000);
+  //
+  //  digitalWrite(in1, HIGH);
+  //  digitalWrite(in2, LOW);
+  //  digitalWrite(in3, LOW);
+  //  digitalWrite(in4, HIGH);
+  //
+  //  delay(800);
+  //
+  //
+  //
+  //
+  //
+  //  digitalWrite(in1, LOW);
+  //  digitalWrite(in2, LOW);
+  //  digitalWrite(in3, LOW);
+  //  digitalWrite(in4, LOW);
+  //  delay(1000);
+
+
+
+
+
+
+
+
+
+//სამკუთხედი
+
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+  delay(700);  //წინ
+
+
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+  delay(1000); //მარჯვნივ
+
+
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+  delay(700); //წინ
+  //  digitalWrite(in1, LOW);
+  //  digitalWrite(in2, HIGH);
+  //  digitalWrite(in3, LOW);
+  //  digitalWrite(in4, HIGH);
+  //  delay(700);
+
+
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+  delay(1200); //უკან
+
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+  delay(1000); //წინ
+
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+  delay(1000);
+
+
+
+}
